6c66213f7d0e5760158b7b7d267dcb9a317b5d0b
Nyphakosi <nyphakosi@gmail.com>
2026-09-24 15:20:33 -0700

    delayed copycat, simpleton

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ enum Strategy {
     Greedy,
     Periodic(u32, u32), // period, seq
     Copycat,
+    DelayedCopycat(usize), // how many turns delay
     Copykitten,
     Anticat,
     Grudger,
@@ -24,16 +25,17 @@ enum Strategy {
     Thankful,
     CautiousThankful,
     Tester,
-    Betrayer,
+    Betrayer(f64), // proportion of rounds to share
     Pavlov,
     Antipavlov,
-    Thoughtful,
+    Businessman,
     CultLeader,
     Cultist(u8),
     Peasant(u8),
     King,
     Index(u8), // one of 32 with single-round memory
     Chancecat(f64, f64), // chance to act like copycat, chance to share
+    Simpleton,
 }
 
 impl Strategy {
@@ -59,18 +61,18 @@ impl Strategy {
             ForgivingGrudger => oppmoves.iter().filter(|b| !*b).count() < 2, // grudges if opponent steals twice
             Thankful => oppmoves.contains(&true), // greedy, unless opponent shares, then generous, aka antigrudger
             CautiousThankful => !oppmoves.iter().filter(|b| !*b).count() < 2, // thankful if opponent shares twice
-            Tester => match oppmoves.len() { // share steal share share, if on round 3 opponent share, become greedy, else become copycat
-                0 => true, 1 => false, 2 => true, 3 => true,
-                _ => if oppmoves[2] { false } else { *oppmoves.last().unwrap_or(&true) }
+            Tester if mymoves.len() < 4 => [true, false, true, true][mymoves.len()],
+            Tester => { // share steal share share, if on round 3 opponent share, become greedy, else become copycat
+                if oppmoves[2] { false } else { *oppmoves.last().unwrap_or(&true) }
             },
-            Betrayer => {if mymoves.len() < ((ROUNDS*8/10) as usize) { // act like copycat, but in the last 20% of rounds, swap to greedy
+            Betrayer(p) => {if mymoves.len() < ((ROUNDS as f64 * p) as usize) { // act like copycat for the first p% of rounds, swap to greedy
                     *oppmoves.last().unwrap_or(&true)
                 } else {false}
             }
             Pavlov => mymoves.last().unwrap_or(&true) == oppmoves.last().unwrap_or(&true),
             Antipavlov => mymoves.last().unwrap_or(&false) != oppmoves.last().unwrap_or(&false),
-            Thoughtful if oppmoves.len() < 8 => [true, true, false, true, true, false, true, true][oppmoves.len()],
-            Thoughtful => { // probabilistic model, makes decisions depending on payout chance
+            Businessman if oppmoves.len() < 8 => [true, true, false, true, true, false, true, true][oppmoves.len()],
+            Businessman => { // probabilistic model, makes decisions depending on payout chance
                 const PAYRATE: f64 = 0.5;
                 const Z: f64 = 1.5;
                 let n = (oppmoves.len() - 1) as f64;
@@ -122,23 +124,23 @@ impl Strategy {
             },
             Cultist(x) if oppmoves.len() < CULTIST_KEY.len() => CULTIST_KEY[oppmoves.len()],
             Cultist(_x) => { // if paired against CultLeader, become generous, else become greedy
-                if {
+                ({
                     let mut sum_ones = 0;
                     for i in 0..CULT_LEADER_KEY.len() {
                         if oppmoves[i] ^ CULT_LEADER_KEY[i] {sum_ones += 1}
                     }
                     sum_ones
-                } < 2 {true} else {false} // if key matches all but one flipped bit, still allow it
+                } < 2) // if key matches all but one flipped bit, still allow it
             },
             Peasant(_x) if mymoves.len() < PEASANT_KEY.len() => PEASANT_KEY[mymoves.len()],
             Peasant(_x) => { // if paired against Peasant, become generous, else become greedy
-                if {
+                ({
                     let mut sum_ones = 0;
                     for i in 0..PEASANT_KEY.len() {
                         if oppmoves[i] ^ PEASANT_KEY[i] {sum_ones += 1}
                     }
                     sum_ones
-                } < 2 {true} else {false} // if key matches all but one flipped bit, still allow it
+                } < 2) // if key matches all but one flipped bit, still allow it
             },
             King if mymoves.len() < PEASANT_KEY.len() => PEASANT_KEY[mymoves.len()],
             King => false,
@@ -158,6 +160,9 @@ impl Strategy {
                     rand::random_bool(*ps)
                 }
             },
+            Simpleton => (!*oppmoves.last().unwrap_or(&true)) ^ (*mymoves.last().unwrap_or(&true)),
+            DelayedCopycat(d) if mymoves.len() < *d => true,
+            DelayedCopycat(d) => oppmoves[oppmoves.len()-*d],
         }
     }
 }
@@ -171,6 +176,7 @@ impl std::fmt::Debug for Strategy {
             Greedy => write!(f, "Greedy"), 
             Periodic(p, b) => {let p = *p as usize; write!(f, "Periodic ({b:0p$b})")}, 
             Copycat => write!(f, "Copycat"), 
+            DelayedCopycat(d) => write!(f, "Delayed Copycat ({d})"), 
             Copykitten => write!(f, "Copykitten"), 
             Anticat => write!(f, "Anticat"), 
             Grudger => write!(f, "Grudger"), 
@@ -178,16 +184,17 @@ impl std::fmt::Debug for Strategy {
             Thankful => write!(f, "Thankful"), 
             CautiousThankful => write!(f, "Cautious Thankful"), 
             Tester => write!(f, "Tester"), 
-            Betrayer => write!(f, "Betrayer"), 
+            Betrayer(p) => write!(f, "Betrayer ({p})"), 
             Pavlov => write!(f, "Pavlov"), 
             Antipavlov => write!(f, "Antipavlov"), 
-            Thoughtful => write!(f, "Thoughtful"), 
+            Businessman => write!(f, "Businessman"), 
             CultLeader => write!(f, "CultLeader"), 
             Cultist(n) => write!(f, "Cultist {n}"), 
             Peasant(n) => write!(f, "Peasant {n}"), 
             King => write!(f, "King"), 
             Index(n) => write!(f, "Index {n}"), 
             Chancecat(pc, ps) => write!(f, "Chancecat ({pc}, {ps})"), 
+            Simpleton => write!(f, "Simpleton"), 
         }
     }
 }
@@ -202,10 +209,13 @@ fn main() {
          Random(0.25), Random(0.50), Random(0.75), 
          Generous, Greedy, 
          //Periodic(2, 0b10), Periodic(2, 0b01), Periodic(4, 0b1100), Periodic(10, 0b1111100000), 
-         Copycat, Copykitten, Anticat, 
+         Copycat, Copykitten, Anticat, Simpleton, 
+         DelayedCopycat(2), DelayedCopycat(4), DelayedCopycat(8), 
          Grudger, ForgivingGrudger, Thankful, CautiousThankful, 
-         Tester, Betrayer, Pavlov, Antipavlov, Thoughtful, 
-         Chancecat(0.25, 0.5), Chancecat(0.5, 0.5),
+         Tester, 
+         Betrayer(0.25), Betrayer(0.50), Betrayer(0.75),
+         Pavlov, Antipavlov, Businessman, 
+         Chancecat(0.25, 0.5), Chancecat(0.5, 0.5), Chancecat(0.75, 0.5), Chancecat(0.90, 0.0),
          CultLeader, King,
          ];
     for i in 0..8 {
