20f6f48189812df2275eba63031c7e78cf4cbaf7
Nyphakosi <nyphakosi@gmail.com>
2026-09-25 00:20:00 -0700

    leniant strategy; FGrudger, CThankful, Copykitten

---
+++ src/strategies/Raindrops/leniant.rs
@@ -0,0 +1,48 @@
+use std::fmt;
+use crate::Strategy;
+
+#[derive(Clone)]
+pub struct ForgivingGrudger(u32); // >=2 = grudging
+impl fmt::Display for ForgivingGrudger {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "ForgivingGrudger")
+    }
+}
+impl Strategy for ForgivingGrudger { // generous, unless opponent steals, then greedy
+    fn decide(&mut self, _memory: &[bool], history: &[bool]) -> bool {
+        if !*history.last().unwrap_or(&true) {self.0 += 1} // if opponent steals, switch to grudging state
+        self.0 < 2
+    }
+}
+
+#[derive(Clone)]
+pub struct CautiousThankful(u32); // >=2 = thanking
+impl fmt::Display for CautiousThankful {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "CautiousThankful")
+    }
+}
+impl Strategy for CautiousThankful { // greedy, unless opponent steals twice, then generous
+    fn decide(&mut self, _memory: &[bool], history: &[bool]) -> bool {
+        if *history.last().unwrap_or(&true) {self.0 += 1} // if opponent shares, switch to thanking state
+        self.0 >= 2
+    }
+}
+
+#[derive(Clone)]
+pub struct Copykitten;
+impl fmt::Display for Copykitten {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Copykitten")
+    }
+}
+impl Strategy for Copykitten {// steals only if opponent steals twice in a row
+    fn decide(&mut self, _memory: &[bool], history: &[bool]) -> bool {
+        if history.len() < 2 {return true}
+        history[history.len()-1] | history[history.len()-2]
+    }
+}
+
+pub(super) fn retrieve_strategies() -> Vec<&'static dyn Strategy> {
+    vec![&ForgivingGrudger(0), &CautiousThankful(0), &Copykitten]
+}
--- src/strategies/Raindrops/mod.rs
+++ src/strategies/Raindrops/mod.rs
@@ -3,10 +3,12 @@ use crate::Strategy;
 mod pavlov;
 mod businessman;
 mod chancecat;
+mod leniant;
 
 pub(super) fn retrieve_strategies() -> Vec<&'static dyn Strategy> {
     let mut strats = pavlov::retrieve_strategies();
     strats.extend(&businessman::retrieve_strategies());
     strats.extend(&chancecat::retrieve_strategies());
+    strats.extend(&leniant::retrieve_strategies());
     strats
 }
